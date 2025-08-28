37a8f239a3a2b783025505762da2de6a702a7767
cesar <cesargustavo53@gmail.com>
2025-08-28 20:17:31 -0300

    add tasks module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { SqlServerProvider } from './database/providers/sql-server.provider';
 import { EnvironmentModule } from './environment/environment.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-	imports: [EnvironmentModule.forRoot(), DatabaseModule.forRoot(SqlServerProvider), AuthModule],
+	imports: [EnvironmentModule.forRoot(), DatabaseModule.forRoot(SqlServerProvider), AuthModule, TasksModule],
 })
 export class AppModule {}
