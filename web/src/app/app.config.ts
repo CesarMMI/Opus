5ad721377cdd33e48e8f9c6ce7990cedc7bacf34
cesar <cesargustavo53@gmail.com>
2025-09-13 10:22:19 -0300

    remoção override do material

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -4,11 +4,9 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
-import { provideNativeDateAdapter } from '@angular/material/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { tokenInterceptor } from './auth/interceptors/token.interceptor';
-import { provideMaterialOverride } from './core/material-override/material-override';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,7 +14,5 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([tokenInterceptor])),
-    provideNativeDateAdapter(),
-    provideMaterialOverride(),
   ],
 };
